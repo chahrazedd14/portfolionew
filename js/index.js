bfd85545fd393dda11e0d362caf1df1931d880e8
chahrazedd14 <shera.mng@gmail.com>
2022-03-30 17:21:06 +0200

    explor page

--- js/index.js
+++ js/index.js
@@ -119,13 +119,3 @@ function onMouseHoverOut() {
     scale: 1
   });
 }
-console.clear();
-
-const elApp = document.querySelector('#app');
-const elStateNav = document.querySelectorAll('[data-to]');
-
-Array.from(elStateNav).forEach(el => {
-  el.addEventListener('click', () => {
-    elApp.dataset.state = el.dataset.to;
-  });
-});
